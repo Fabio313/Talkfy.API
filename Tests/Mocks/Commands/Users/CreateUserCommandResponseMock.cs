ba1f541281d257050969cbae9c14ca4d04ba27f2
GMBrandao <gabrielmartbrandao@gmail.com>
2024-10-14 21:08:59 -0300

    testes da criacao do usuario

---
+++ Tests/Mocks/Commands/Users/CreateUserCommandResponseMock.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using Domain.Commands.v1.Users.CreateUser;
+
+namespace Tests.Mocks.Commands.Users
+{
+    public static class CreateUserCommandResponseMock
+    {
+        public static CreateUserCommandResponse GenerateDefault()
+        {
+            return new Faker<CreateUserCommandResponse>()
+                .RuleFor(x => x.Id, faker => faker.Random.Guid().ToString())
+                .RuleFor(x => x.Username, faker => faker.Random.String2(15))
+                .RuleFor(x => x.Email, faker => faker.Internet.Email())
+                .RuleFor(x => x.CreatedDate, faker => faker.Date.Recent(1));
+        }
+    }
+}
